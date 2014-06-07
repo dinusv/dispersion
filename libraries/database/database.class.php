e2b81412171d025df62538480b4463284a971434
Dinu SV <mail@dinusv.com>
2014-06-07 18:51:40 +0300

    Switched licensing header on all files to comply with current website.

--- libraries/database/database.class.php
+++ libraries/database/database.class.php
@@ -1,12 +1,14 @@
 <?php  if ( ! defined('ROOT')) exit('Script access is forbidden');
-/*                                                 **
-| This file is part of Inevy Dispersion Framework.  |
-| http://dispersion.inevy.com                       |
-|                                                   |
-| License : http://dispersion.inevy.com/license     |
-|                                                   |
-| Copyright 2010-2011 (c) inevy                     |
-** -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  */
+/****************************************************************************
+**
+** Copyright (C) 2010-2014 Dinu SV.
+** (contact: mail@dinusv.com)
+** This file is part of Dispersion framework.
+** 
+** The file may be used under the terms of the MIT license, appearing in the
+** file LICENSE.MIT included in the packaging of this file.
+**
+****************************************************************************/
 
 /**  Database abstraction, containing all the defined methods
  * 
